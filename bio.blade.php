662f0c01fc9ad650c3699226aec550f770fc65ca
habueb87 <95731826+habueb87@users.noreply.github.com>
2022-03-06 23:41:53 +0700

    Add files via upload

---
+++ bio.blade.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+    <title>Bio</title>
+    <link rel="stylesheet" href="css/bioPg.css">
+</head>
+<body class="bg">
+    <div class="tb">
+    <div class="img"></div>
+    <br><br><br>
+    <table border="5" cellpadding="5" align="center" width="700" height="250">
+        <tr>
+            <td bgcolor="silver">Nama</td>
+            <td>Satria Alief Pratama Sofyan</td>            
+        </tr>
+        <tr>
+            <td bgcolor="silver">Tempat/Tangal Lahir</td>
+            <td>Malang, 06-06-2002</td>
+        </tr>
+        <tr>
+            <td bgcolor="silver">Alamat</td>
+            <td>Joyogrand Merjosari</td>
+        </tr>
+        <tr>
+            <td bgcolor="silver">Agama</td>
+            <td>Islam</td>
+        </tr>
+        <tr>
+            <td bgcolor="silver">Jenis Kelamin</td>
+            <td>Laki-Laki</td>
+        </tr>
+        <tr>
+            <td bgcolor="silver">Okupasi</td>
+            <td>Mahasiswa</td>
+        </tr>
+        <tr>
+            <td bgcolor="silver">Hobby</td>
+            <td>Gunpla</td>
+        </tr>
+    </table>
+    </div>
+    <footer class="text">@Copyright SATR Digital</footer>
+</body>
+</html>
